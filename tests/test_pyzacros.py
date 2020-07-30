08740c311253e09b777e09fe42884e6e174f7918
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-07-30 10:01:02 +0200

    Fix testing.

--- tests/test_pyzacros.py
+++ tests/test_pyzacros.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Tests for the pyZacros module.
-"""
-import pytest
-import sys
+"""Tests for the pyZacros module."""
 
-from pyzacros.classes.kmc import KmcSimulation
+from pyzacros.classes.Kmc import KmcSimulation
 from pyzacros.classes.Species import Species
 from pyzacros.classes.SpeciesList import SpeciesList
 from pyzacros.classes.Cluster import Cluster
 from pyzacros.classes.ElementaryReaction import ElementaryReaction
 from pyzacros.classes.Mechanism import Mechanism
 
-"""
-Tests on classes.
-"""
 
-Species.test()
-SpeciesList.test()
-Cluster.test()
-ElementaryReaction.test()
-Mechanism.test()
-
-
-def test_with_error():
-    with pytest.raises(ValueError):
-        # Do something that raises a ValueError
-        raise(ValueError)
+def test_classes():
+    """Testing of classes."""
+    Species.test()
+    SpeciesList.test()
+    Cluster.test()
+    ElementaryReaction.test()
+    Mechanism.test()
 
 
 # Fixture example
-@pytest.fixture
-def an_object():
-    return {}
-
-
-def test_pyZacros(an_object):
-    assert an_object == {}
+#@pytest.fixture
+#def an_object():
+#    return {}
+#
+#
+#def test_pyZacros(an_object):
+#    assert an_object == {}
+#
