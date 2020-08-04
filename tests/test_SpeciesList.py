c3750b71f4d71be611a3207863ae0eba6edf1f80
Nestor F. Aguirre <nfaguirrec@gmail.com>
2020-08-04 19:30:36 +0200

    1) New test for Job class 2) Tests updated
    Funding: SCM ReaxPro EU203

--- tests/test_SpeciesList.py
+++ tests/test_SpeciesList.py
@@ -8,29 +8,29 @@ from pyzacros.classes.SpeciesList import SpeciesList
 
 def test_SpeciesList():
     """Test of the SpeciesList class."""
-    print("---------------------------------------------------")
-    print(">>> Testing SpeciesList class")
-    print("---------------------------------------------------")
+    print( "---------------------------------------------------" )
+    print( ">>> Testing SpeciesList class" )
+    print( "---------------------------------------------------" )
 
     # Adsorbed specie
-    myAdsorbedSpecies1 = Species("H2*", denticity=1)
-    myAdsorbedSpecies2 = Species("O2*", denticity=1)
+    myAdsorbedSpecies1 = Species( "H2*", denticity=1 )
+    myAdsorbedSpecies2 = Species( "O2*", denticity=1 )
 
     # Gas specie
-    myGasSpecies1 = Species("H2", gas_energy=0.0)
+    myGasSpecies1 = Species( "H2", gas_energy=0.0 )
 
     # Gas specie
-    myGasSpecies2 = Species("O2", gas_energy=0.0)
+    myGasSpecies2 = Species( "O2", gas_energy=0.0 )
 
     # Free adsorption site
-    myFreeAdsorptionSite = Species("*")
+    myFreeAdsorptionSite = Species( "*" )
 
     mySpeciesList = SpeciesList()
-    mySpeciesList.append(myAdsorbedSpecies1)
-    mySpeciesList.append(myAdsorbedSpecies2)
-    mySpeciesList.append(myGasSpecies1)
-    mySpeciesList.append(myGasSpecies2)
-    mySpeciesList.append(myFreeAdsorptionSite)
+    mySpeciesList.append( myAdsorbedSpecies1 )
+    mySpeciesList.append( myAdsorbedSpecies2 )
+    mySpeciesList.append( myGasSpecies1 )
+    mySpeciesList.append( myGasSpecies2 )
+    mySpeciesList.append( myFreeAdsorptionSite )
 
     print(mySpeciesList)
 
@@ -41,8 +41,8 @@ gas_specs_names           H2        O2
 gas_energies             0.0       0.0
 gas_molec_weights        XXX       XXX
 
-n_surf_species 3
-surf_specs_names         H2*       O2*         *
-surf_specs_dent            1         1         0\
+n_surf_species 2
+surf_specs_names         H2*       O2*
+surf_specs_dent            1         1\
 """
-    assert(output == expectedOutput)
+    assert( output == expectedOutput )
