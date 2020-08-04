2e74568054c3ebd9991a14462f790745a3b6bd6c
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-08-04 14:43:32 +0200

    Create test_Species.py

--- tests/test_Species.py
+++ tests/test_Species.py
@@ -8,3 +8,30 @@ from pyzacros.classes.Species import Species
 
 def test_Species():
     """Test of the Species class."""
+    print("---------------------------------------------------")
+    print(">>> Testing Species class")
+    print("---------------------------------------------------")
+
+    # Adsorbed specie
+    myAdsorbedSpecies = Species("H2*", denticity=1)
+    print(myAdsorbedSpecies)
+
+    output = str(myAdsorbedSpecies)
+    expectedOutput = "H2*"
+    assert(output == expectedOutput)
+
+    # Gas specie
+    myGasSpecies = Species("H2", gas_energy=0.0)
+    print(myGasSpecies)
+
+    output = str(myGasSpecies)
+    expectedOutput = "H2"
+    assert(output == expectedOutput)
+
+    # Free adsorption site
+    myAdsorptionFreeSite = Species("*")
+    print(myAdsorptionFreeSite)
+
+    output = str(myAdsorptionFreeSite)
+    expectedOutput = "*"
+    assert(output == expectedOutput)
