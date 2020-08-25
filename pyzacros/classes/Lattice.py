a75c5d32673c4c32bd20256b8c1d96d7fd0f79e0
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-08-25 15:55:19 +0200

    Adding test_Lattice.py

--- pyzacros/classes/Lattice.py
+++ pyzacros/classes/Lattice.py
@@ -62,7 +62,7 @@ class Lattice():
 
     def __str__(self) -> str:
         """Translate the object to a string."""
-        output = "lattice"+"\t"+self.lattice_type+"\n\n"
+        output = "lattice"+"\t"+self.lattice_type+"\n"
 
         output += "cell_vectors"
         for i in range(2):
@@ -70,31 +70,31 @@ class Lattice():
             for j in range(2):
                 output += "\t"+str(self.cell_vectors[i][j])
 
-        output += "\n \nrepeat_cell\t"
+        output += "\nrepeat_cell\t"
         output += str(str(self.repeat_cell)[1:-1]).replace(',', '')
 
-        output += "\n \nn_cell_sites\t"+str(self.n_cell_sites)
+        output += "\nn_cell_sites\t"+str(self.n_cell_sites)
 
         output += "\nn_site_types\t"+str(self.n_site_types)
 
         output += "\nsite_type_names\t" \
                   + str(' '.join(str(x) for x in self.site_type_names))
 
-        output += "\n \nsite_types\t" \
+        output += "\nsite_types\t" \
                   + str(' '.join(str(x) for x in self.site_types))
 
-        output += "\n \nsite_coordinates\n"
+        output += "\nsite_coordinates"
         for i in range(self.n_cell_sites):
             output += "\n"
             for j in range(2):
                 output += "\t"+str(self.site_coordinates[i][j])
 
-        output += "\n \nneighboring_structure\n"
+        output += "\nneighboring_structure"
         for i in range(len(self.neighboring_structure)):
             output += "\n"
             output += "\t"+str(' '.join(str(self.neighboring_structure[i][j])
                                for j in range(2)))
-        output += "\n \nend_neighboring_structure"
-        output += "\n \nend_lattice"
+        output += "\nend_neighboring_structure"
+        output += "\nend_lattice"
 
         return output
