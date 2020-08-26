0af3c179d6caa2201b56c935914e449315d66d63
Nestor F. Aguirre <nfaguirrec@gmail.com>
2020-08-26 09:55:30 +0200

    The first trial of interactivity with PLAMS

--- pyzacros/classes/Mechanism.py
+++ pyzacros/classes/Mechanism.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from .ElementaryReaction import *
 
 class Mechanism(list):
@@ -15,3 +18,58 @@ class Mechanism(list):
         output += "end_mechanism"
 
         return output
+
+
+    #def fromAMS( self, amsResultsDirName: str="ams.results" ):
+    #def fromAMS( self, result: scm.plams.Results ):
+    def fromAMS( self, results ):
+        """
+        Loads the mechanism from a AMS results object
+
+        :parm amsResultsDirName: str. Stablishes the file name of the rkf file to load
+        :parm results: str. xxxx
+        """
+        AMSHOME = os.getenv("AMSHOME")
+        if( AMSHOME == None ):
+            print( "### Error ###: Environment variable AMSHOME not found !!!" )
+            quit()
+        if( AMSHOME+"/scripting" not in sys.path ): sys.path.append( AMSHOME+"/scripting" )
+
+        try:
+            import scm.plams
+        except ImportError:
+            pass
+
+        scm.plams.init()
+
+        print("\n-------------------------------------")
+        print(" Results")
+        print("-------------------------------------\n")
+
+        nStates = results.readrkf("EnergyLandscape", "nStates")
+        fileNames = results.readrkf("EnergyLandscape", "fileNames").replace(".rkf","").split()
+        counts = results.readrkf("EnergyLandscape", "counts")
+        isTS = results.readrkf("EnergyLandscape", "isTS")
+        reactants = results.readrkf("EnergyLandscape", "reactants")
+        products = results.readrkf("EnergyLandscape", "products")
+
+        print("nStates = ", nStates)
+        print("fileNames = ", fileNames)
+        print("counts = ", counts)
+        print("isTS = ", isTS)
+        print("reactants = ", reactants)
+        print("products = ", products)
+
+        for i,fileName in enumerate(fileNames):
+            mol = results.get_molecule("Molecule", file=fileNames[i])
+            skeleton = results.get_rkf_skeleton( file=fileNames[i] )
+            amsResults = results.read_rkf_section("AMSResults", file=fileNames[i])
+            thermodynamics = results.read_rkf_section("Thermodynamics", file=fileNames[i])
+            vibrations = results.read_rkf_section("Vibrations", file=fileNames[i])
+            print("Id = ", i)
+            print("Energy = ", amsResults["Energy"])
+            print("Frequencies = ", vibrations["Frequencies[cm-1]"])
+            print("ZeroPointEnergy = ", vibrations["ZeroPointEnergy"])
+            print(mol)
+
+        scm.plams.finish()
---
+++ tests/test_MechanismFromAMS.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests of the pyZacros classes."""
+
+import os
+import sys
+
+from pyzacros.classes.Species import Species
+from pyzacros.classes.SpeciesList import SpeciesList
+from pyzacros.classes.Cluster import Cluster
+from pyzacros.classes.ElementaryReaction import ElementaryReaction
+from pyzacros.classes.Mechanism import Mechanism
+from pyzacros.classes.AMSResultsLoader import AMSResultsLoader
+
+def test_MechanismFromAMS():
+    """Test of the Mechanism class loaded from AMS."""
+    print( "---------------------------------------------------" )
+    print( ">>> Testing AMSResultsLoader class" )
+    print( "---------------------------------------------------" )
+
+    AMSHOME = os.getenv("AMSHOME")
+    if( AMSHOME == None ):
+        print( "### Error ###: Environment variable AMSHOME is required !!!" )
+        quit()
+    if( AMSHOME+"/scripting" not in sys.path ): sys.path.append( AMSHOME+"/scripting" )
+
+    try:
+        import scm.plams
+    except ImportError:
+        pass
+
+    scm.plams.init()
+
+    molecule = scm.plams.Molecule( "tests/4H2O.xyz" )
+
+    settings = scm.plams.Settings()
+    settings.input.ams.Task = 'BasinHopping-EON'
+    settings.input.MOPAC
+    settings.input.ams.Properties.NormalModes = "T"
+
+    settings.input.ams.EON.RandomSeed = 100
+    settings.input.ams.EON.Temperature = 400.0
+    settings.input.ams.EON.SamplingFreq = "Full"
+
+    settings.input.ams.EON.BasinHopping.Steps = 10
+    settings.input.ams.EON.BasinHopping.Displacement = 1.0
+    settings.input.ams.EON.BasinHopping.WriteUnique = "T"
+
+    settings.input.ams.EON.Optimizer.Method = "CG"
+    settings.input.ams.EON.Optimizer.ConvergedForce = 0.01
+    settings.input.ams.EON.Optimizer.MaxIterations = 300
+
+    settings.input.ams.EON.StructureComparison.DistanceDifference = 0.1
+    settings.input.ams.EON.StructureComparison.NeighborCutoff = 7.6
+    settings.input.ams.EON.StructureComparison.CheckRotation = "T"
+    settings.input.ams.EON.StructureComparison.IndistinguishableAtoms = "T"
+    settings.input.ams.EON.StructureComparison.EnergyDifference = 0.01
+    settings.input.ams.EON.StructureComparison.RemoveTranslation = "T"
+
+    job1 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='BasinHopping')
+    print( job1.get_input() )
+    results = job1.run()
+
+    settings.input.ams.Task = 'NudgedElasticBand-EON'
+    settings.input.ams.EON.LoadStates.File = results.rkfpath()
+
+    job2 = job1
+    job2 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='NudgedElasticBand')
+    print( job2.get_input() )
+    results = job2.run()
+
+    myMechanism = Mechanism()
+    myMechanism.fromAMS( results )
+
+    print( myMechanism )
+
+    scm.plams.finish()
