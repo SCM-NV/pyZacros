44f54df960198f066624914b1d6d9ef9636aef4c
Nestor F. Aguirre <nfaguirrec@gmail.com>
2020-09-06 01:19:42 +0200

    Improving interaction with AMS
    Funding: SCM ReaxPro EU203

--- tests/test_MechanismFromAMS.py
+++ tests/test_MechanismFromAMS.py
@@ -18,59 +18,59 @@ def test_MechanismFromAMS():
     print( "---------------------------------------------------" )
 
     AMSHOME = os.getenv("AMSHOME")
-    if( AMSHOME == None ):
-        print( "### Error ###: Environment variable AMSHOME is required !!!" )
-        quit()
-    if( AMSHOME+"/scripting" not in sys.path ): sys.path.append( AMSHOME+"/scripting" )
+    if( AMSHOME is not None ):
+        if( AMSHOME+"/scripting" not in sys.path ): sys.path.append( AMSHOME+"/scripting" )
 
-    try:
-        import scm.plams
-    except ImportError:
-        pass
+        try:
+            import scm.plams
+        except ImportError:
+            pass
 
-    scm.plams.init()
+        scm.plams.init()
 
-    molecule = scm.plams.Molecule( "tests/4H2O.xyz" )
+        molecule = scm.plams.Molecule( "tests/4H2O.xyz" )
 
-    settings = scm.plams.Settings()
-    settings.input.ams.Task = 'BasinHopping-EON'
-    settings.input.MOPAC
-    settings.input.ams.Properties.NormalModes = "T"
+        settings = scm.plams.Settings()
+        settings.input.ams.Task = 'BasinHopping-EON'
+        settings.input.MOPAC
+        settings.input.ams.Properties.NormalModes = "T"
 
-    settings.input.ams.EON.RandomSeed = 100
-    settings.input.ams.EON.Temperature = 400.0
-    settings.input.ams.EON.SamplingFreq = "Full"
+        settings.input.ams.EON.RandomSeed = 100
+        settings.input.ams.EON.Temperature = 400.0
+        settings.input.ams.EON.SamplingFreq = "Full"
 
-    settings.input.ams.EON.BasinHopping.Steps = 10
-    settings.input.ams.EON.BasinHopping.Displacement = 1.0
-    settings.input.ams.EON.BasinHopping.WriteUnique = "T"
+        settings.input.ams.EON.BasinHopping.Steps = 10
+        settings.input.ams.EON.BasinHopping.Displacement = 1.0
+        settings.input.ams.EON.BasinHopping.WriteUnique = "T"
 
-    settings.input.ams.EON.Optimizer.Method = "CG"
-    settings.input.ams.EON.Optimizer.ConvergedForce = 0.01
-    settings.input.ams.EON.Optimizer.MaxIterations = 300
+        settings.input.ams.EON.Optimizer.Method = "CG"
+        settings.input.ams.EON.Optimizer.ConvergedForce = 0.01
+        settings.input.ams.EON.Optimizer.MaxIterations = 300
 
-    settings.input.ams.EON.StructureComparison.DistanceDifference = 0.1
-    settings.input.ams.EON.StructureComparison.NeighborCutoff = 7.6
-    settings.input.ams.EON.StructureComparison.CheckRotation = "T"
-    settings.input.ams.EON.StructureComparison.IndistinguishableAtoms = "T"
-    settings.input.ams.EON.StructureComparison.EnergyDifference = 0.01
-    settings.input.ams.EON.StructureComparison.RemoveTranslation = "T"
+        settings.input.ams.EON.StructureComparison.DistanceDifference = 0.1
+        settings.input.ams.EON.StructureComparison.NeighborCutoff = 7.6
+        settings.input.ams.EON.StructureComparison.CheckRotation = "T"
+        settings.input.ams.EON.StructureComparison.IndistinguishableAtoms = "T"
+        settings.input.ams.EON.StructureComparison.EnergyDifference = 0.01
+        settings.input.ams.EON.StructureComparison.RemoveTranslation = "T"
 
-    job1 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='BasinHopping')
-    print( job1.get_input() )
-    results = job1.run()
+        job1 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='BasinHopping')
+        print( job1.get_input() )
+        results = job1.run()
 
-    settings.input.ams.Task = 'NudgedElasticBand-EON'
-    settings.input.ams.EON.LoadStates.File = results.rkfpath()
+        settings.input.ams.Task = 'NudgedElasticBand-EON'
+        settings.input.ams.EON.LoadStates.File = results.rkfpath()
 
-    job2 = job1
-    job2 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='NudgedElasticBand')
-    print( job2.get_input() )
-    results = job2.run()
+        job2 = job1
+        job2 = scm.plams.AMSJob(molecule=molecule, settings=settings, name='NudgedElasticBand')
+        print( job2.get_input() )
+        results = job2.run()
 
-    myMechanism = Mechanism()
-    myMechanism.fromAMS( results )
+        myMechanism = Mechanism()
+        myMechanism.fromAMS( results )
 
-    print( myMechanism )
+        print( myMechanism )
 
-    scm.plams.finish()
+        scm.plams.finish()
+
+test_MechanismFromAMS()
