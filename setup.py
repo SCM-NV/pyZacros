f7b120229911cbc4333bca4b7303f65b40d6274e
Bas Rustenburg <rustenburg@scm.com>
2021-10-07 14:26:43 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+packages = ['scm.pyzacros'] + ['scm.pyzacros.'+i for i in find_packages('.')]
+
+
 # To update the package version number, edit pyZacros/__version__.py
 version = {}
 with open(os.path.join(here, 'pyzacros', '__version__.py')) as f:
@@ -21,21 +24,18 @@ setup(
     author="Pablo Lopez-Tarifa",
     author_email='p.lopez@esciencecenter.nl',
     url='https://github.com/NLeSC/pyZacros',
-    packages=find_packages(),
+    packages=packages,
     include_package_data=True,
-    license="Apache Software License 2.0",
+    license="LGPLv3",
     zip_safe=False,
     keywords='pyZacros',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.6',        
     ],
 
     install_requires=[
