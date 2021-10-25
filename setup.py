d87f6b448c0da225e65e35fde6b8a28842408561
Bas Rustenburg <rustenburg@scm.com>
2021-10-25 17:20:08 +0200

    Update project structure for merge into SVN stack.

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+packages = ['scm.pyzacros'] + ['scm.pyzacros.'+i for i in find_packages('.')]
+
 # To update the package version number, edit pyZacros/__version__.py
 version = {}
-with open(os.path.join(here, 'pyzacros', '__version__.py')) as f:
+with open(os.path.join(here, '__version__.py')) as f:
     exec(f.read(), version)
 
 with open('README.rst') as readme_file:
@@ -21,21 +23,20 @@ setup(
     author="Pablo Lopez-Tarifa",
     author_email='p.lopez@esciencecenter.nl',
     url='https://github.com/NLeSC/pyZacros',
-    packages=find_packages(),
+    packages=packages,
+    package_dir = {'scm.pyzacros': '.'},
+    package_data={'scm.pyzacros' : ['lattice_db/*', 'examples/*']},
     include_package_data=True,
-    license="Apache Software License 2.0",
+    license="LGPLv3",
     zip_safe=False,
     keywords='pyZacros',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.6',        
     ],
 
     install_requires=[
