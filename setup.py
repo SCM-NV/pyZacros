c3c2ca76c25b51209bd4ad98a42d8e77507a1efb
Nestor F. Aguirre <nfaguirrec@gmail.com>
2021-11-03 11:28:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,25 +24,26 @@ setup(
     author_email='p.lopez@esciencecenter.nl',
     url='https://github.com/SCM-NV/pyZacros',
     packages=packages,
-    package_dir = {'pyzacros': '.'},
-    package_data={'pyzacros' : ['examples/*', 'tests/*']},
+    package_dir = {'scm.pyzacros': '.'},
+    package_data={'scm.pyzacros' : ['examples/*', 'tests/*']},
     include_package_data=True,
     license="LGPLv3",
     zip_safe=False,
-    keywords='pyZacros',
+    keywords = ['molecular modeling', 'computational chemistry', 'workflow', 'python interface'],
     classifiers=[
-        'Development Status :: Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
     install_requires=[
         'pyaml', 'chemparse',
         'plams@git+https://github.com/SCM-NV/PLAMS@master'],
-
     extras_require={
         'test': ['coverage', 'pytest>=3.9', 'pytest-cov'],
         'doc': ['sphinx', 'sphinx_rtd_theme', 'nbsphinx']
