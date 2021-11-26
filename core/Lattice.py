Several changes: 1. Calculation of the coverage fractions 2. Improved plotting options 3. Few bugs fixed 4. Documentation updated 5. New example ZGB-PhaseTransitions Funding: SCM ReaxPro EU203

core/Lattice.py
@@ -273,7 +273,7 @@ class Lattice:
 
         def getcellnumber(i, j):
             if( i < 0 or j < 0 or i >= repeat_cell[0] or j >= repeat_cell[1] ):
-                return None 
+                return None
             return i*repeat_cell[1] + j
 
         v1 = cell_vectors[0]
@@ -443,7 +443,8 @@ class Lattice:
             yvalues = [ y for (x,y),st in zip(self.site_coordinates,self.site_types) if st==st_i ]
 
             lcolor = color if color is not None else colors[i]
-            ax.scatter(xvalues, yvalues, color=lcolor, marker=markers[i], s=100, zorder=2, label=st_i)
+            ax.scatter(xvalues, yvalues, color=lcolor, marker=markers[i],
+                       s=440/math.sqrt(len(self.site_coordinates)), zorder=2, label=st_i)
 
             if( show_sites_ids ):
                 for i,(x,y) in enumerate(self.site_coordinates):
@@ -464,7 +465,8 @@ class Lattice:
                     if( norm > np.linalg.norm(1.5*np.array(v2)) ): continue
 
                 lcolor = color if color is not None else 'k'
-                ax.plot(xvalues, yvalues, color=lcolor, linestyle='solid', linewidth=0.5, zorder=1)
+                ax.plot(xvalues, yvalues, color=lcolor, linestyle='solid',
+                        linewidth=1.5/math.sqrt(len(self.site_coordinates)), zorder=1)
 
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.tight_layout()

core/ZacrosResults.py
@@ -235,7 +235,7 @@ class ZacrosResults( scm.plams.Results ):
         return names
 
 
-    def lattice_states(self):
+    def lattice_states(self, last=None):
         """
         Return the configurations from the 'history_output.txt' file.
         """
@@ -277,7 +277,13 @@ class ZacrosResults( scm.plams.Results ):
                     surface_species[i] = sp
         surface_species = SpeciesList( surface_species )
 
-        for nconf in range(number_of_snapshots-prev_number_of_snapshots):
+        number_of_snapshots_to_load = number_of_snapshots-prev_number_of_snapshots
+
+        llast = number_of_snapshots_to_load
+        if( last is not None ): llast= last
+        if( llast < 0 ): llast = 0
+
+        for nconf in range(number_of_snapshots_to_load-llast,number_of_snapshots_to_load):
             lines = self.grep_file(self._filenames['history'], pattern='configuration', options="-A"+str(number_of_lattice_sites)+" -m"+str(nconf+1))
             lines = lines[-number_of_lattice_sites-1:] # Equivalent to tail -n $number_of_lattice_sites+1
 
@@ -315,61 +321,7 @@ class ZacrosResults( scm.plams.Results ):
         """
         Return the last configuration from the 'history_output.txt' file.
         """
-        number_of_lattice_sites = self.number_of_lattice_sites()
-        number_of_snapshots = self.number_of_snapshots()
-
-        # In the following lines, I just check the consistency of the gas species and
-        # surface species between the general and history files.
-        lines = self.grep_file(self._filenames['history'], pattern='Gas_Species:')
-        gas_species_names = lines[0][ lines[0].find('Gas_Species:')+len("Gas_Species:"): ].split()
-
-        assert gas_species_names == self.gas_species_names(), "Warning: Inconsistent gas species between "+ \
-            self._filenames['history']+" and "+self._filenames['general']
-
-        lines = self.grep_file(self._filenames['history'], pattern='Surface_Species:')
-        surface_species_names = lines[0][ lines[0].find('Surface_Species:')+len("Surface_Species:"): ].split()
-
-        assert surface_species_names == self.surface_species_names(), "Warning: Inconsistent surface species between "+ \
-            self._filenames['history']+" and "+self._filenames['general']
-
-        surface_species = len(surface_species_names)*[None]
-        for i,sname in enumerate(surface_species_names):
-            for sp in self.job.mechanism.surface_species():
-                if( sname == sp.symbol ):
-                    surface_species[i] = sp
-        surface_species = SpeciesList( surface_species )
-
-        nconf = number_of_snapshots
-
-        lines = self.grep_file(self._filenames['history'], pattern='configuration', options="-A"+str(number_of_lattice_sites)+" -m"+str(nconf+1))
-        lines = lines[-number_of_lattice_sites-1:] # Equivalent to tail -n $number_of_lattice_sites+1
-
-        lattice_state = None
-        for nline,line in enumerate(lines):
-            tokens = line.split()
-
-            if( nline==0 ):
-                assert tokens[0] == "configuration"
-
-                configuration_number = int(tokens[1])
-                number_of_events = int(tokens[2])
-                time = float(tokens[3])
-                temperature = float(tokens[4])
-                energy = float(tokens[5])
-
-                add_info = {"number_of_events":number_of_events, "time":time,
-                            "temperature":temperature, "energy":energy}
-                lattice_state = LatticeState( self.job.lattice, surface_species, add_info=add_info )
-            else:
-                site_number = int(tokens[0])-1 # Zacros uses arrays indexed from 1
-                adsorbate_number = int(tokens[1])
-                species_number = int(tokens[2])-1 # Zacros uses arrays indexed from 1
-                dentation = int(tokens[3])
-
-                if( species_number > -1 ): # In zacros -1 means empty site (0 for Zacros)
-                    lattice_state.fill_site( site_number, surface_species[species_number] )
-
-        return lattice_state
+        return self.lattice_states(last=1)[0]
 
 
     def plot_lattice_states(self, data, pause=-1, show=True, ax=None, close=False, time_perframe=0.5, file_name=None):
@@ -661,7 +613,7 @@ class ZacrosResults( scm.plams.Results ):
 
     def get_TOFs(self, npoints=None):
         """
-        Return the TOF calculated with the last ```npoints```
+        Return the TOF (mol/sec/site) calculated with the last ```npoints```
 
         *   ``npoints`` --
         """
@@ -694,7 +646,7 @@ class ZacrosResults( scm.plams.Results ):
 
             popt,pcov = scipy.optimize.curve_fit(line, tVec, nMolsVec)
 
-            values[sn] = popt[0]
+            values[sn] = popt[0]/self.number_of_lattice_sites()
             errors[sn] = math.sqrt(pcov[0,0])
 
         return values,errors