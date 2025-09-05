17f2a4841e80cb9ed0f0f6b6adfe721fa46cc6a1
Nestor Aguirre <nfaguirrec@gmail.com>
2025-09-06 00:19:32 +0200

    Fixing the RKFLoader after UCS update -- SO--

--- tests/test_RKFLoader.py
+++ tests/test_RKFLoader.py
@@ -22,7 +22,7 @@ def generateAMSResults(test_folder):
     sett_ads.input.ams.PESExploration.DynamicSeedStates = "T"
     sett_ads.input.ams.PESExploration.StructureComparison.DistanceDifference = 0.1
     sett_ads.input.ams.PESExploration.StructureComparison.NeighborCutoff = 3.8
-    sett_ads.input.ams.PESExploration.StructureComparison.EnergyDifference = 0.05
+    sett_ads.input.ams.PESExploration.StructureComparison.EnergyDifference = 0.5
     sett_ads.input.ams.PESExploration.StructureComparison.CheckSymmetry = "T"
     sett_ads.input.ams.PESExploration.CalculateFragments = "T"
     sett_ads.input.ams.PESExploration.StatesAlignment.ReferenceRegion = "surface"
